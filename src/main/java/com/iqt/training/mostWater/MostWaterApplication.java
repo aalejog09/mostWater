db9886a5bdc1bbc79447696bbe7748cddbe6ba7a
Andres Alejo <aalejog09@gmail.com>
2024-04-23 08:49:45 -0400

    first Commit. Base proyect

---
+++ src/main/java/com/iqt/training/mostWater/MostWaterApplication.java
@@ -0,0 +1,13 @@
+package com.iqt.training.mostWater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostWaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostWaterApplication.class, args);
+	}
+
+}
